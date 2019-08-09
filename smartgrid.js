9371e629fb26a7cbafd54e1fe75421a88990ee34
ko7ovz <51297745+ko7ovz@users.noreply.github.com>
2019-08-09 12:48:25 -0700

    Create smartgrid.js

---
+++ smartgrid.js
@@ -0,0 +1,30 @@
+module.exports = {
+    columns: 12,
+        offset: "30px",
+        outputStyle: "styl",
+        container: {
+            maxWidth: "none",
+            fields: "30px"
+        },
+        breakPoints: {
+            lg: {
+                width: "1200px"
+            },
+            md: {
+                width: "992px",
+                fields: "25px"
+            },
+            sm: {
+                width: "720px",
+                fields: "20px"
+            },
+            xs: {
+                width: "576px",
+                fields: "15px"
+            },
+            xxs: {
+                width: "320px",
+                fields: "10px"
+            }
+        }
+}
